2e8fa61de829da45749d37298ef222ad2c6c6a31
Aurora Sky <62398803+melissa-clothilde@users.noreply.github.com>
2020-05-10 00:09:41 -0700

    submits while continuing to edit the get local storage part

--- script.js
+++ script.js
@@ -8,42 +8,52 @@ var currentTime = m.format('HH');
 var time = $('.left');
 var activityText = $('#block1');
 
-
-timeBlock.each(function (index) {
-    $(this).attr('data-time', index + 9);
-    console.log($(this).attr('data-time'));
-});
-
-
-timeBlock.each(function () {
-    if (parseInt($(this).attr('data-time')) === parseInt(currentTime)) {
-        $(this).css('background-color', 'red');
-    }
-
-    else if (parseInt($(this).attr('data-time')) > parseInt(currentTime)) {
-        $(this).css('background-color', 'green');
-    }
-
-    else {
-        $(this).css('background-color', 'grey');
-    }
-});
-
+//the current day is displayed at the top of the calendar
 dayDiv.text(m.format("dddd" + ", " + "MMM Mo"));
 
 
-saveBtn.each(function(index) {
+//each timeblock is color coded to indicate whether it is in the past, present, or future
+timeBlock.each(function (index) {
+    $(this).attr('data-time', index + 9);
+});
+
+timeBlock.each(function () {
+    if (parseInt($(this).attr('data-time')) === parseInt(currentTime)) {
+        $(this).addClass("present");
+    }
+
+    else if (parseInt($(this).attr('data-time')) > parseInt(currentTime)) {
+        $(this).addClass("future");
+    }
+
+    else {
+        $(this).addClass("past");
+    }
+});
+
+//when user clicks the save button for that timeblock, the text for that event is saved in local storage
+saveBtn.each(function (index) {
     $(this).attr('data-textboxId', '#block' + index);
 })
 
-saveBtn.on("click", function() {
+saveBtn.on("click", function () {
     var textboxId = $(this).attr('data-textboxId');
-    console.log(textboxId);
     var textInput = $(textboxId);
     textInput.val();
-    taskArray.push(textInput.val());
-    localStorage.setItem('taks', JSON.stringify(taskArray));
-   
+    tasksArray.push(textInput.val());
+    localStorage.setItem('tasks', JSON.stringify(tasksArray));
 });
 
-var taskArray = [];
+var tasksArray = [];
+
+//when user refreshes the page, the saved events persist
+//an attempt...
+$(document).ready(function listTasks () {
+        var taskArray = localStorage.getItem(JSON.parse('tasks'));
+        if (!task) {
+            taskArray = [];
+        }
+        $.each(taskArray, function (value) {
+            timeBlock.append(value);
+        });
+    });
